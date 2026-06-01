b1bdcd6fe561673d0eac4365482e53bacd3176c7
Duncan Forgan <dh4gan@gmail.com>
2026-06-01 21:29:36 +0100

    Switch to a light theme as default.  Add bio image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="home">    
     <h1>Dr Duncan Forgan PhD</h1>
-    <p>🚧 Welcome to my new website, which is currently under construction</p>    
+
+    <img
+    width="400"
+    src="../assets/Elevate-1521-crop.png"
+    alt="A picture of Duncan.  He is wearing orange glasses, a pink shirt and a tweed blazer" />
+    <p>Hi, I'm Duncan. I've been on a bit of a journey over my career.</p>    
     <p>I was a computational astrophysicist and astrobiologist for ten years, specialising in the formation of planets and the search for extraterrestrial intelligence (SETI).  You can find my book on the subject, <a href="https://www.cambridge.org/core/books/solving-fermis-paradox/46945FE622E1F62E8DA36832B9F58A8A">Solving Fermi's Paradox</a>, at all good bookstores.</p>
-    <p>Since then, I've moved on into software engineering industry, as a Technical Lead at Dayshape.</p>
-    <p>I'm also a keen sci-fi writer and a member of the Edinburgh Science Fiction and Fantasy Discord (<a href="https://edinburghsff.com/">ESFF</a>)</p>
+    <p>Since then, I've moved on into software engineering industry.  I'm now a Technical Lead at Dayshape, leading a team of engineers to improve the working lives of people in professional services.</p>
+    <p>I'm a keen sci-fi writer and a member of the Edinburgh Science Fiction and Fantasy Discord (<a href="https://edinburghsff.com/">ESFF</a>)</p>
     <div class="nav-links">
       <router-link to="/blog" class="btn blog">Read My Blog Posts</router-link>
     </div>
